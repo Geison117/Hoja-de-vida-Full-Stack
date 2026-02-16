8edb12975ff71c36eed06ee6392c9f2a83e5d7e6
Geison Blanco <geisonblanco94@gmail.com>
2026-02-16 06:18:32 -0500

    Update authService.tsx

--- Frontend/resume_frontend/src/services/authService.tsx
+++ Frontend/resume_frontend/src/services/authService.tsx
@@ -1,5 +1,7 @@
 import api from "../api/axios";
 
+import api from "../api/axios";
+
 export const login = async (email, password) => {
   try {
     const response = await api.post("/auth/login", {
@@ -11,14 +13,6 @@ export const login = async (email, password) => {
     console.error(error);
     throw error;  // para que quien llame a login pueda manejar el error
   }
-};
-
-  const { access_token } = response.data;
-
-  // Guardar token
-  localStorage.setItem("token", access_token);
-
-  return response.data;
 };
 
 export const getCurrentUser = async () => {
