bd1ad672b5ce34899d4832d99c13100438d8a881
Geison Blanco <geisonblanco94@gmail.com>
2026-02-16 05:00:18 -0500

    Update vite.config.ts

--- Frontend/resume_frontend/vite.config.ts
+++ Frontend/resume_frontend/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://hoja-de-vida-full-stack.onrender.com/",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, "")
