d4dd777de54d5d80e9326ee2644c0203824a84a9
Geison Blanco <geisonblanco94@gmail.com>
2026-02-16 21:32:26 -0500

    Update axiosClient.tsx

--- Frontend/resume_frontend/src/api/axiosClient.tsx
+++ Frontend/resume_frontend/src/api/axiosClient.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api"//import.meta.env.VITE_API_URL, //"/api", // usando proxy de Vite
+  baseURL: import.meta.env.VITE_API_URL || "/api", // usando proxy de Vite
 });
 
 // 🔐 Interceptor para agregar token automáticamente
